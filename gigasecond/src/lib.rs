7b87e2d0a71bf6aae71a35febf2b942e93c655f5
Prince Muel <vansomecsam@gmail.com>
2025-01-15 03:35:35 +0100

    feat: solve gigasecond exercise

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,10 +1,8 @@
-use time::PrimitiveDateTime as DateTime;
+use time::{Duration, PrimitiveDateTime as DateTime};
 
-// // const KILO_SEC: u32 = 1_000;
-// // const MEGA_SEC: u32 = 1_000_000;
-// // const GIGA_SEC: u32 = 1_000_000_000;
+const GIGA_SEC: i64 = 1_000_000_000;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    // todo!("What time is a gigasecond later than {start}");
+    start + Duration::seconds(GIGA_SEC)
 }
